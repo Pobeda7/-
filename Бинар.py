77a48beb2349dc3eb09bfe792088ea78c55a2148
Pobeda7 <116107245+Pobeda7@users.noreply.github.com>
2022-11-20 21:48:36 +0300

    Create Бинар.py

---
+++ Бинар.py
@@ -0,0 +1,35 @@
+RawInput = input('Введите последовательность чисел через пробел: ')
+int_array = [int(i) for i in RawInput.split (' ') if i.isdigit()]
+list.sort(int_array)
+print(int_array)
+
+num = input('Введите число: ')
+print(num)
+
+
+def binary_search(int_array, num, left, right):
+    if left > right:
+        return False
+    middle = (right + left) // 2
+    if int_array[middle] == num:
+        return middle
+    elif num < int_array[middle]:
+
+        return binary_search(int_array, num, left, middle - 1)
+    else:
+        return binary_search(int_array, num, right, middle + 1)
+
+while True:
+    try:
+        num = int(input('Введите число: '))
+        if num < 0 or num > 999:
+            raise Exception
+        break
+    except ValueError:
+        print('Введите вероное число')
+    except Exception:
+        print('Не диапазон')
+
+int_array = [i for i in range(1, 100)]
+
+print(binary_search(int_array, num, 0, len (int_array)))
