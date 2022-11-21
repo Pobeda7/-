cc0e3e94e1c9037b26aa56faf662f29037e47155
Pobeda7 <116107245+Pobeda7@users.noreply.github.com>
2022-11-21 12:18:52 +0300

    Update Бинар.py

--- Бинар.py
+++ Бинар.py
@@ -11,23 +11,10 @@ def binary_search(int_array, num, left, right):
         return False
     middle = (right + left) // 2
     if int_array[middle] == num:
-        return middle 
+        return middle -1
     elif num < int_array[middle]:
         return binary_search(int_array, num, left, middle - 1)
     else:
         return binary_search(int_array, num, right, middle + 1)
-
-# while True:
-#     try:
-#         num = int(input('Введите число: '))
-#         if num < 0 or num > 999:
-#             raise Exception
-#         break
-#     except ValueError:
-#         print('Введите вероное число')
-#     except Exception:
-#         print('В не диапазон')
-
-# int_array = [i for i in range(1, 100)]
-
+        
 print(f'Индекс числа:', (binary_search(int_array, num, 0, len(int_array)-1)))
