082198f112e65388dd418fa1af1df384a522ced6
Pobeda7 <116107245+Pobeda7@users.noreply.github.com>
2022-11-21 11:20:50 +0300

    Update Бинар.py

--- Бинар.py
+++ Бинар.py
@@ -3,33 +3,31 @@ int_array = [int(i) for i in RawInput.split (' ') if i.isdigit()]
 list.sort(int_array)
 print(int_array)
 
-num = input('Введите число: ')
+num = int(input('Введите число: '))
 print(num)
-
 
 def binary_search(int_array, num, left, right):
     if left > right:
         return False
     middle = (right + left) // 2
     if int_array[middle] == num:
-        return middle
+        return middle 
     elif num < int_array[middle]:
-
         return binary_search(int_array, num, left, middle - 1)
     else:
         return binary_search(int_array, num, right, middle + 1)
 
-while True:
-    try:
-        num = int(input('Введите число: '))
-        if num < 0 or num > 999:
-            raise Exception
-        break
-    except ValueError:
-        print('Введите вероное число')
-    except Exception:
-        print('В не диапазон')
+# while True:
+#     try:
+#         num = int(input('Введите число: '))
+#         if num < 0 or num > 999:
+#             raise Exception
+#         break
+#     except ValueError:
+#         print('Введите вероное число')
+#     except Exception:
+#         print('В не диапазон')
 
-int_array = [i for i in range(1, 100)]
+# int_array = [i for i in range(1, 100)]
 
-print(binary_search(int_array, num, 0, len (int_array)))
+print(f'Индекс числа:', (binary_search(int_array, num, 0, len(int_array)-1)))
